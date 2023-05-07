562ac04e643e09155888236a2cd4e0459bc9fadc
Zijie Ji <ge64lum@mytum.de>
2023-05-07 23:08:49 +0800

    after challenge1

---
+++ CMakeFiles/foo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C:/Users/my precious/ESPLss23/bin/foo.exe"
+  "C:/Users/my precious/ESPLss23/bin/foo.exe.manifest"
+  "C:/Users/my precious/ESPLss23/bin/foo.pdb"
+  "CMakeFiles/foo.dir/src/hello.c.obj"
+  "foo.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/foo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
